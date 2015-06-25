9984179ccff42167a0d449529897ac866df6257b
andrewflowers <andrew.w.flowers@gmail.com>
2015-06-25 15:55:28 -0400

    update code and add documentation

--- inequality/CE-survey.R
+++ inequality/CE-survey.R
@@ -15,11 +15,16 @@ setwd("~/data-analysis/inequality")
 # Load expn file
 load("./microdata/2013/expn/fra13.rda")
 load("./microdata/2013/intrvw/fmli131x.rda")
+load("./microdata/2013/intrvw/fmli132.rda")
+load("./microdata/2013/intrvw/fmli133.rda")
+load("./microdata/2013/intrvw/fmli134.rda")
+fmli13 <- rbind(fmli131x, fmli132, fmli133, fmli134)
 
 # Join income category to expn file
 fra13$inclass <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
-
+fra13$inclass <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
 sofas <- fra13 %>% filter(furnpury==100, furngftc==1) %>% 
   group_by(inclass) %>% 
-  summarize(median=median(as.numeric(furnpurx)))
+  summarize(n=n(),
+            median=median(as.numeric(furnpurx)))
 
