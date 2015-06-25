17d4494d5b82e7b6ec2ad08d754c085fb2531f81
andrewflowers <andrew.w.flowers@gmail.com>
2015-06-25 12:32:07 -0400

    updated code

--- inequality/CE-survey.R
+++ inequality/CE-survey.R
@@ -7,7 +7,19 @@ require(readr)
 require(dplyr)
 require(ggplot2)
 
-# Import data
+setwd("~/data-analysis/inequality")
 
+# Example data analysis
+# Sofas, specifically
 
+# Load expn file
+load("./microdata/2013/expn/fra13.rda")
+load("./microdata/2013/intrvw/fmli131x.rda")
+
+# Join income category to expn file
+fra13$inclass <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
+
+sofas <- fra13 %>% filter(furnpury==100, furngftc==1) %>% 
+  group_by(inclass) %>% 
+  summarize(median=median(as.numeric(furnpurx)))
 
