f8f8df53aac2bd5e9dec3ab64891a1836fa888f0
Andrew Flowers <andrew.w.flowers@gmail.com>
2015-07-21 13:10:30 -0400

    updated script

--- inequality/CE-survey.R
+++ inequality/CE-survey.R
@@ -6,6 +6,9 @@
 require(readr)
 require(dplyr)
 require(ggplot2)
+require(reshape2)
+require(cwhmisc)
+require(xlsx)
 
 setwd("~/data-analysis/inequality")
 
@@ -22,51 +25,51 @@ load("./microdata/2013/intrvw/fmli141.rda")
 
 # Extract data on all 2013 CU's -- newid, inclass2, etc.
 vars <- c("newid", "inclass2", "finlwt21")
-consumers <- rbind(fmli131[,vars], fmli131x[,vars], fmli132[,vars], fmli133[,vars], fmli134[,vars], fmli141[,vars])
+consumers <- rbind(fmli131x[,vars], fmli132[,vars], fmli133[,vars], fmli134[,vars], fmli141[,vars])
 
 # APA (appliances) expn file
 items <- c(100, 120, 140, 160, 190, 200)
-itemDesc <- c("stove", "microwave", "refrigerator", "dishwasher", "washer", "dryer")
+itemDesc <- as.data.frame(c("stove", "microwave", "refrigerator", "dishwasher", "washer", "dryer"))
+colnames(itemDesc) <- "item"
 # NOTE: need to deal with combined appliance purchases, where MAJAPPY = 220 or 225
 
 appPurchases <- apa13 %>% 
+  mutate(weight=consumers[match(apa13$newid, consumers$newid),]$finlwt21) %>%
   mutate(inclass2=consumers[match(apa13$newid, consumers$newid),]$inclass2) %>% 
   mutate(incTercile=ifelse(inclass2<=2, "bottom", ifelse(inclass2<=4 & inclass2>2, "middle", ifelse(inclass2<7 & inclass2>4, "top", "none")))) %>% 
-  filter(majapply %in% items, gftc_maj==1)
+  filter(majapply %in% items, gftc_maj==1, incTercile!="none") %>% 
+  select(newid, majapply, weight, majpurx, incTercile) 
+
+appReport <- appPurchases %>% 
+  dcast(formula=majapply~incTercile, value.var="majpurx", fun.aggregate=median, fill=NaN) %>%
+  # NOTE: this median calculation is UNWEIGHTED
+  cbind(itemDesc) %>% select(item, 2:4)
+
+# FRA (furniture) expn file
+items <- c(100, 101, 102, 103, 120, 141, 171, 190, 191, 192, 193, 214, 215)
+itemDesc <- as.data.frame(c("sofas", "living room chairs", "living room tables", 
+                            "shelves/cabinets", "mattresses/springs", "bbq grill", 
+                            "lamps", "plastic dinnerware", "china dinnerware", "stainless/silver flatware",
+                            "glassware", "curtains/drapes", "blinds/shades"
+                            ))
+colnames(itemDesc) <- "item"
+# NOTE: need to deal with combined furniture purchases, where FURNNPURY = 106, 122, 160, 197, 204, 220, 900
+
+furPurchases <- fra13 %>% 
+  mutate(weight=consumers[match(fra13$newid, consumers$newid),]$finlwt21) %>%
+  mutate(inclass2=consumers[match(fra13$newid, consumers$newid),]$inclass2) %>% 
+  mutate(incTercile=ifelse(inclass2<=2, "bottom", ifelse(inclass2<=4 & inclass2>2, "middle", ifelse(inclass2<7 & inclass2>4, "top", "none")))) %>% 
+  filter(furnpury %in% items, furngftc==1, incTercile!="none") %>% 
+  select(newid, furnpury, weight, furnpurx, incTercile) 
+
+furReport <- furPurchases %>% 
+  dcast(formula=furnpury~incTercile, value.var="furnpurx", fun.aggregate=median, fill=NaN) %>%
+  # NOTE: this median calculation is UNWEIGHTED
+  cbind(itemDesc) %>% select(item, 2:4)
 
 
-
-
-
-
-# Join income category to expn files
-fra13$inclass <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
-fra13$inclass <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
-sofas <- fra13 %>% filter(furnpury==100, furngftc==1) %>% 
-  group_by(inclass) %>% 
-  summarize(n=n(),
-            median=median(as.numeric(furnpurx)))
-
-# Alternative income classification
-fra13$inclass2 <- fmli131x[match(fra13$newid, fmli131x$newid),]$inclass
-
-# Use inclass2, but just 
-# example: table(fmli131x$inclass2)
-
-# Weights: use final weight, finlwt21
-
-
-# Kitchen/living room items to classify:
-# FRA
-# sofa (100)
-# APA
-# 
-
-
-items <- c(100)
-
-
-
+finalReport <- rbind(appReport, furReport)
+write.xlsx(finalReport, "kitchen_and_living_room.xlsx", sheetName="report")
 
 
 
