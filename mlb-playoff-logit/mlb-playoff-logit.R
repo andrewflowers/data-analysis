643da9ec4e179d2e15332c5571c0f14c476b3b72
NeilPaine538 <neil.paine@gmail.com>
2015-07-30 12:49:48 -0400

    update proj

--- mlb-playoff-logit/mlb-playoff-logit.R
+++ mlb-playoff-logit/mlb-playoff-logit.R
@@ -1,8 +1,10 @@
 # Neil Paine
 #setwd()
 
-reg <- glm(made_playoffs~DOFF+at_time_pyth+preseason_talent,family=binomial(link="logit"))
+data  <- read.csv("rawdata.tsv", sep="\t")
+
+reg <- glm(data=data, made_playoffs~DOFF+at_time_pyth+preseason_talent,family=binomial(link="logit"))
 
 summary(reg)
 
-plot(reg$fitted ~ factor(made_playoffs))
+plot(data$fitted ~ factor(data$made_playoffs))
