0a0fa9aa212c0b121e5f484f87b716342ea31e0b
Andrew Flowers <andrew.w.flowers@gmail.com>
2015-08-03 19:18:46 -0400

    updated code

--- tlc-foil-data/data-analysis.R
+++ tlc-foil-data/data-analysis.R
@@ -6,8 +6,15 @@ setwd("~/data-analysis/tlc-foil-data/")
 require(readr)
 require(dplyr)
 require(ggplot2)
+require(tidyr)
+require(stringr)
+require(lubridate)
 
 uberData <- read_csv("uber-raw-data.csv")
-uberData2 <- read_csv("Uber-Jan-Feb-FOIL.csv")
 
-lyftData <- read_csv("Lyft_B02510.csv")
+bases <- data.frame(base_name=c('Unter', 'Hinter', 'Weiter', 'Schmecken', 'Danach-NY'),
+                    base_code=c('B02512', 'B02598', 'B02617', 'B02682', 'B02764'))
+
+uberData$base_name <- bases[match(uberData$Base, bases$base_code),]$base_name
+
+head(uberData) %>% str_split(`Date/Time`, " ")
