d4d1dcd7bfb0ea92381412fdafc6a3431bcd7af2
Andrew Flowers <andrew.w.flowers@gmail.com>
2015-08-03 10:29:18 -0400

    update proj

--- super-PACs/super-PACs-distribution.R
+++ super-PACs/super-PACs-distribution.R
@@ -1,6 +1,7 @@
 # Super PACs Distribution
 
 setwd("~/data-analysis/super-PACs/")
+source("~/data-analysis/Five38Thm.R")
 
 require(readr)
 require(dplyr)
@@ -15,9 +16,16 @@ dist <- bycoffe2015 %>% group_by(supports) %>%
          bottom5=ifelse(total<=threshold2, TRUE, FALSE)) %>% 
   summarize(total_raised=sum(total),
             avg_donor=total_raised/n(),
+            n_top5=sum(ifelse(top5, 1, 0)),
             from_top5=sum(ifelse(top5, total, 0)), 
+            n_bottom5=sum(ifelse(bottom5, 1, 0)),
             from_bottom5=sum(ifelse(bottom5, total, 0)), 
-            ratio=from_top5/from_bottom5) %>% 
+            ratio=from_top5/from_bottom5,
+            top5_share=(from_top5/total_raised)*100) %>% 
   arrange(desc(ratio))
 
+ggplot(data=dist, aes(x=avg_donor, y=ratio, label=supports))+
+  geom_point()+geom_text()+xlab("Average SuperPAC Donation")+ylab("Ratio of top 5% to bottom 5%")
 
+ggplot(data=dist, aes(x=n_top5, y=top5_share, label=supports))+
+  geom_point()+geom_text(vjust=-1)+xlab("Number of donors in top 5%")+ylab("Percentage from top 5% of donors") + Five38Thm
